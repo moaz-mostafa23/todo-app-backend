2fe7a6b230df9b5bef0293a54098d3871a7db92a
Moaz Mostafa <m.mostafa@calo.app>
2025-05-10 15:52:33 +0300

    add ddb table

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,6 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class TodoAppBackendStack extends Stack {
   public readonly userPool: cognito.UserPool;
@@ -9,6 +10,8 @@ export class TodoAppBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+
+    // COGNITO
     this.userPool = new cognito.UserPool(this, 'TodoUserPool', {
       selfSignUpEnabled: true,
       signInAliases: {
@@ -28,6 +31,29 @@ export class TodoAppBackendStack extends Stack {
     new cognito.CfnUserPoolDomain(this, 'UserPoolDomain', {
       domain: 'todo-app-' + this.stackName.toLowerCase(),
       userPoolId: this.userPool.userPoolId,
+    });
+
+    new CfnOutput(this, 'UserPoolIdOutput', {
+      value: this.userPool.userPoolId,
+      exportName: 'CognitoUserPoolId',
+    });
+
+    new CfnOutput(this, 'UserPoolClientIdOutput', {
+      value: this.userPoolClient.userPoolClientId,
+      exportName: 'CognitoUserPoolClientId',
+    });
+
+    //DDB
+    const todosTable = new dynamodb.Table(this, 'TodosTable', {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'todoId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY, // deletes table on cdk destroy
+    });
+
+    new CfnOutput(this, 'TodosTableNameOutput', {
+      value: todosTable.tableName,
+      exportName: 'TodosTableName',
     });
   }
 }
